16f822cfac27f9beef98f7dad45d09de4cda537e
sarawut014-4 <sarawut-won@rmutp.ac.th>
2025-08-27 17:41:36 +0700

    Delete dist

--- dist/app/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var node_server_1 = require("@hono/node-server");
-var index_1 = require("./index");
-(0, node_server_1.serve)(index_1.default, function (info) {
-    console.log("Server is running on ".concat(info.port));
-});
-//# sourceMappingURL=server.js.map
