6ce44e605ba95447a6305726047dbe663c4ff17f
sarawut wongmanee <sarawut-won@rmutp.ac.th>
2025-09-03 16:44:03 +0700

    fix bcrypt

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
+import * as bcrypt from "bcrypt";
 //import { PrismaClient } from "../generated/prisma/Client";
 
 const prisma = new PrismaClient();
@@ -17,4 +18,19 @@ const app = new Hono();
      return c.json(profile);
  });
 
+app.post("/profile", async (c) => {
+    //login to create a new profile 
+    const body = await c.req.json();
+    console.log('input of profile', body);
+    console.log('body.password(original)', body.password);
+    
+    //encode password
+    const passwordHash = await bcrypt.hash(body.password, 18);
+    console.log('hash.password(after)', passwordHash);
+    //save to db 
+    //output response 
+    return c.json({
+        message: "create profile completed"
+    });
+});
 export default app; 
