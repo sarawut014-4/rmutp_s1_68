2ec306b69602f12dd334232ff8d7b4f48c4c1b85
sarawut wongmanee <sarawut-won@rmutp.ac.th>
2025-09-03 17:31:45 +0700

    update

--- app/index.ts
+++ app/index.ts
@@ -25,15 +25,20 @@ app.post("/profile", async (c) => {
     console.log('body.password(original)', body.password);
     
     //encode password
-    const passwordHash = await bcrypt.hash(body.password, 18);
+    const passwordHash = await bcrypt.hash(body.password, 13);
     console.log('hash.password(after)', passwordHash);
     body.password = passwordHash;
     console.log('body.password(replace) ', body);
 
     //save to db 
+    body.status= false;
+    const result = await prisma.profile.create({
+        data: body
+    })
     //output response 
     return c.json({
-        message: "create profile completed"
+        message: "create profile completed",
+        data: result 
     });
 });
 export default app; 
