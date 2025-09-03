94c142ab8bcd2511ffd0df04d8403e76bb5ed41f
sarawut wongmanee <sarawut-won@rmutp.ac.th>
2025-09-03 16:52:44 +0700

    add newbody

--- app/index.ts
+++ app/index.ts
@@ -27,6 +27,9 @@ app.post("/profile", async (c) => {
     //encode password
     const passwordHash = await bcrypt.hash(body.password, 18);
     console.log('hash.password(after)', passwordHash);
+    body.password = passwordHash;
+    console.log('body.password(replace) ', body);
+
     //save to db 
     //output response 
     return c.json({
